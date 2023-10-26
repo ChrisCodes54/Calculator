ecc9698043c7bb47d787a7b80b73a0fca26ea3d0
Christopher Tangarife <christophertangarife@christophersmbp.lan>
2023-10-26 15:08:33 -0400

    styling is fine for now, responsiveness might need to be worked on. Created a function to not repeat bootstrap styling code for all numbers

--- src/components/Buttons/index.js
+++ src/components/Buttons/index.js
@@ -1,37 +1,51 @@
 import React from "react";
 import "./index.css";
+import 'bootstrap/dist/css/bootstrap.css';
+
+function buttonStyle(props) {
+    const {numLabel} = props;
+    return (
+        <button className={`col btn btn-warning rounded-0 border border-dark`}>
+            {numLabel}
+        </button>
+    )
+}
+
 export default function Buttons() {
   return (
-    <>
-      <container className="container">
+    <div className="d-flex align-items-center justify-content-center vh-100"> 
+    <div className="w-50">
+      <div className="container">
         <div className="row">
-          <button className="col">7</button>
-          <button className="col">8</button>
-          <button className="col">9</button>
+          {buttonStyle({numLabel: "7"})}
+          {buttonStyle({numLabel: "8"})}
+          {buttonStyle({numLabel: "9"})}
+          
           <button className="col">x</button>
         </div>
 
         <div className="row">
-          <button className="col">4</button>
-          <button className="col">5</button>
-          <button className="col">6</button>
+          <button className="col btn btn-warning rounded-0 border border-dark">4</button>
+          <button className="col btn btn-warning rounded-0 border border-dark">5</button>
+          <button className="col btn btn-warning rounded-0 border border-dark">6</button>
           <button className="col">-</button>
         </div>
 
         <div className="row">
-          <button className="col">1</button>
-          <button className="col">2</button>
-          <button className="col">3</button>
+          <button className="col btn btn-warning rounded-0 border border-dark">1</button>
+          <button className="col btn btn-warning rounded-0 border border-dark">2</button>
+          <button className="col btn btn-warning rounded-0 border border-dark">3</button>
           <button className="col">+</button>
         </div>
 
-        <div class="row">
-          <button class="col-8">0</button>
+        <div className="row">
+          <button className="col-8 btn btn-warning rounded-0 border border-dark">0</button>
           <button className="col">.</button>
           <button className="col">=</button>
         </div>
 
-      </container>
-    </>
+      </div>
+      </div>
+    </div>
   );
 }
