97af754fb31ab923dfa8521abc56606e31303baf
Christopher Tangarife <christophertangarife@christophersmbp.lan>
2023-10-27 09:37:28 -0400

    created funcitons to grab 1-9 and 0

--- src/components/Buttons/index.js
+++ src/components/Buttons/index.js
@@ -1,50 +1,74 @@
 import React from "react";
 import "./index.css";
-import 'bootstrap/dist/css/bootstrap.css';
+import "bootstrap/dist/css/bootstrap.css";
 
 function buttonStyle(props) {
-    const {numLabel} = props;
-    return (
-        <button className={`col btn btn-warning rounded-0 border border-dark`}>
-            {numLabel}
-        </button>
-    )
+  const { numLabel } = props;
+
+  const handleClick = () => {
+    console.log(numLabel);
+  };
+  return (
+    <button
+      className={`col btn btn-warning rounded-0 border border-dark`}
+      onClick={handleClick}
+    >
+      {numLabel}
+    </button>
+  );
+}
+
+function handleZero(props) {
+  const { numLabel } = props;
+
+  const handleClick = () => {
+    console.log(numLabel);
+  };
+  return (
+    <button
+      className={`col-6 btn btn-warning rounded-0 border border-dark`}
+      onClick={handleClick}
+    >
+      {numLabel}
+    </button>
+  );
 }
 
 export default function Buttons() {
   return (
-    <div className="d-flex align-items-center justify-content-center vh-100"> 
-    <div className="w-50">
-      <div className="container">
-        <div className="row">
-          {buttonStyle({numLabel: "7"})}
-          {buttonStyle({numLabel: "8"})}
-          {buttonStyle({numLabel: "9"})}
-          
-          <button className="col">x</button>
-        </div>
+    <div className="d-flex align-items-center justify-content-center vh-100">
+      <div className="w-50">
+        <div className="container">
+          <div className="row">
+            {buttonStyle({ numLabel: "7" })}
+            {buttonStyle({ numLabel: "8" })}
+            {buttonStyle({ numLabel: "9" })}
 
-        <div className="row">
-          <button className="col btn btn-warning rounded-0 border border-dark">4</button>
-          <button className="col btn btn-warning rounded-0 border border-dark">5</button>
-          <button className="col btn btn-warning rounded-0 border border-dark">6</button>
-          <button className="col">-</button>
-        </div>
+            <button className="col">x</button>
+          </div>
 
-        <div className="row">
-          <button className="col btn btn-warning rounded-0 border border-dark">1</button>
-          <button className="col btn btn-warning rounded-0 border border-dark">2</button>
-          <button className="col btn btn-warning rounded-0 border border-dark">3</button>
-          <button className="col">+</button>
-        </div>
+          <div className="row">
+            {buttonStyle({ numLabel: "4" })}
+            {buttonStyle({ numLabel: "5" })}
+            {buttonStyle({ numLabel: "6" })}
 
-        <div className="row">
-          <button className="col-8 btn btn-warning rounded-0 border border-dark">0</button>
-          <button className="col">.</button>
-          <button className="col">=</button>
-        </div>
+            <button className="col">-</button>
+          </div>
 
-      </div>
+          <div className="row">
+            {buttonStyle({ numLabel: "1" })}
+            {buttonStyle({ numLabel: "2" })}
+            {buttonStyle({ numLabel: "3" })}
+
+            <button className="col">+</button>
+          </div>
+
+          <div className="row">
+            {handleZero({ numLabel: "0" })}
+            <button className="col">.</button>
+            <button className="col">=</button>
+          </div>
+        </div>
       </div>
     </div>
   );
