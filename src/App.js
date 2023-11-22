effbd38901a2320fd2e3c32c182fca15776546b6
Christopher Tangarife <christophertangarife@christophersmbp.lan>
2023-11-22 13:04:12 -0500

    reconfigured import paths for calculator and button, added bottom row to calculator

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./App.css";
-import Calculator from "./components/Calculator";
+import Calculator from "./components/Calculator/Calculator";
+import Button from "./components/Calculator/Button";
 
 function App() {
   return <>
   
-  
+  <Button/>
   <Calculator />
   
   </>;
--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -7,13 +7,14 @@ import "bootstrap/dist/css/bootstrap.css";
 const buttons = [
   [7, 8, 9, "x"],
   [4, 5, 6, "-"],
-  [1, 2, 3, "="],
+  [1, 2, 3, "+"],
+  [0, ".", "="]
 ];
 
 const ops = new Set(["x", "-", "+"]);
 
 export default function Calculator() {
-  const [calculation, setCalculation] = useState("");
+  // const [calculation, setCalculation] = useState("");
   const onClickHandler = (value) => {
     // add logic here
   };
@@ -22,7 +23,7 @@ export default function Calculator() {
       <div className="w-50">
         <div className="container">
           <div className="row">
-            <Display calculation={calculation} />
+            {/* <Display calculation={calculation} /> */}
           </div>
           {buttons.map((buttonRow) => {
             return(
